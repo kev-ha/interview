12b655310255d215214a498e5678590e84acb1b2
Kevin Hariyanto <Kevin@Kevins-MacBook-Pro-2.local>
2022-08-09 20:10:00 -0400

    Added logic

--- word-counter/src/Application.java
+++ word-counter/src/Application.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
+import logic.WordCounter;
+
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		System.out.println("Hello");
+		System.out.println("Word Frequency Counter application. Enter input as a sentence in one line. Type \"exit\" to terminate the app.");
+		WordCounter wc = new WordCounter();
+
+		boolean running = true;
+		do {
+			System.out.println("\nInput: ");
+			Scanner scanner = new Scanner(System.in);
+			String str = scanner.nextLine();
+			
+			wc.getWordFrequency(str);
+			
+			if (str.equalsIgnoreCase("exit")) {
+				scanner.close();
+				running = false;
+				System.out.println("Word Frequency Counter application closed.");
+			}
+		}
+		while (running);
+		
+		System.exit(0);
 	}
 
 }
